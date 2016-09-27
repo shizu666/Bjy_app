d50648c74c776d234b3cb1329846370985388275
zhouguangyue <zhouGUANGYUE666>
2016-09-27 20:03:13 +0800

    update

--- Bjy/wms/src/main/java/app/wms/InventoryQuery.java
+++ Bjy/wms/src/main/java/app/wms/InventoryQuery.java
@@ -2,10 +2,20 @@ package app.wms;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import app.wms.empty.StockLocationRequest;
+
 public class InventoryQuery extends AppCompatActivity {
-    private TextView tv_order_name;
+    private TextView tv_order_name ,tv_iq_name ,tv_iq_num ,tv_iq_unit ,tv_iq_huoweihao,tv_iq_cereateDate ,tv_iq_tiaoshu;
+    private EditText et_iq_huowei , et_iq_guobiaoma;
+    private Button but_iq_up ,but_iq_next ,but_iq_back;
+    private List<StockLocationRequest> list = new ArrayList<StockLocationRequest>();
 
 
     @Override
@@ -14,6 +24,10 @@ public class InventoryQuery extends AppCompatActivity {
         setContentView(R.layout.activity_inventory_query);
         tv_order_name = (TextView) findViewById(R.id.tv_order_name);
         tv_order_name.setText("库存查询");
+        initView();
 
+    }
+
+    private void initView() {
     }
 }
---
+++ Bjy/wms/src/main/java/app/wms/empty/StockLocationRequest.java
@@ -0,0 +1,201 @@
+package app.wms.empty;
+
+import java.util.Date;
+
+/**
+ * Created by zhou on 2016/9/27.
+ */
+
+public class StockLocationRequest {
+
+    /** ID */
+    private Long id;
+    /** SKU */
+    private String sku;
+    /** 货主编号 */
+    private String ownerCode;
+    /** 货主名称 */
+    private String ownerName;
+    /** 仓库 */
+    private String warehouseCode;
+    /** 库区类型 */
+    private Integer areaType;
+    /** 库区 */
+    private String areaCode;
+    /** 巷道 */
+    private String roadCode;
+    /** 货位 */
+    private String locationCode;
+    /** 批次号 */
+    private String batchNo;
+    /** 质量等级 */
+    private Integer qualityLevel;
+    /** 是否赠品 */
+    private Integer isGift;
+    /** 可用库存数量 */
+    private Integer validNum;
+    /** 预占库存数量 */
+    private Integer reservedNum;
+    /** 创建时间 */
+    private Date createTime;
+    /** 创建用户 */
+    private String createUser;
+    /** 修改时间 */
+    private Date updateTime;
+    /** 修改用户 */
+    private String updateUser;
+    /** 有效标识 */
+    private Integer yn;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String getOwnerCode() {
+        return ownerCode;
+    }
+
+    public void setOwnerCode(String ownerCode) {
+        this.ownerCode = ownerCode;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public String getWarehouseCode() {
+        return warehouseCode;
+    }
+
+    public void setWarehouseCode(String warehouseCode) {
+        this.warehouseCode = warehouseCode;
+    }
+
+    public Integer getAreaType() {
+        return areaType;
+    }
+
+    public void setAreaType(Integer areaType) {
+        this.areaType = areaType;
+    }
+
+    public String getAreaCode() {
+        return areaCode;
+    }
+
+    public void setAreaCode(String areaCode) {
+        this.areaCode = areaCode;
+    }
+
+    public String getRoadCode() {
+        return roadCode;
+    }
+
+    public void setRoadCode(String roadCode) {
+        this.roadCode = roadCode;
+    }
+
+    public String getLocationCode() {
+        return locationCode;
+    }
+
+    public void setLocationCode(String locationCode) {
+        this.locationCode = locationCode;
+    }
+
+    public String getBatchNo() {
+        return batchNo;
+    }
+
+    public void setBatchNo(String batchNo) {
+        this.batchNo = batchNo;
+    }
+
+    public Integer getQualityLevel() {
+        return qualityLevel;
+    }
+
+    public void setQualityLevel(Integer qualityLevel) {
+        this.qualityLevel = qualityLevel;
+    }
+
+    public Integer getIsGift() {
+        return isGift;
+    }
+
+    public void setIsGift(Integer isGift) {
+        this.isGift = isGift;
+    }
+
+    public Integer getValidNum() {
+        return validNum;
+    }
+
+    public void setValidNum(Integer validNum) {
+        this.validNum = validNum;
+    }
+
+    public Integer getReservedNum() {
+        return reservedNum;
+    }
+
+    public void setReservedNum(Integer reservedNum) {
+        this.reservedNum = reservedNum;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(String updateUser) {
+        this.updateUser = updateUser;
+    }
+
+    public Integer getYn() {
+        return yn;
+    }
+
+    public void setYn(Integer yn) {
+        this.yn = yn;
+    }
+}
